e23c54ff8e90d0ca020cd223b54112c814bafef3
William Antony <william@willsquad.com>
2018-09-12 22:39:18 -0500

    Created Boilerplate for react-component without redux

---
+++ source/react/component/withoutRedux/MyComponent/MyComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './MyComponent.css';
+
+class MyComponent extends Component {
+
+  render() {
+    return (
+      <div className="MyComponent">
+
+      </div>
+    );
+  }
+
+}
+
+export default MyComponent;
