88d69d7df23d9d3088a28032ecd733555a4426a8
William Antony <william@willsquad.com>
2018-09-15 22:57:08 -0500

    Replaced runInOrder() with its package

--- create-react.js
+++ create-react.js
@@ -2,6 +2,7 @@
 const fs = require('fs');
 
 const { copyAllFiles } = require('@willsquad/fs-utilities');
+const runInOrder = require('@willsquad/run-in-order');
 
 const { yesno } = require('user-input-cli-test');
 const MultipleChoice = require('multiple-choice-cli-test');
@@ -18,47 +19,6 @@ const BgCyan = '\x1b[46m';
 
 const sourcePath = `${ __dirname }/source`;
 
-
-
-const runInOrder = (fn, arguments = []) => {
-
-  return new Promise((resolve, reject) => {
-
-    const recur = () => {
-      
-      const thisArgument = arguments.shift();
-
-      const continueRun = () => {
-
-        if (arguments.length > 0) {
-          recur();
-        } else {
-          resolve();
-        }
-
-      };
-
-      const fnReturned = fn(thisArgument);
-
-      if (fnReturned instanceof Promise) {
-
-        fnReturned
-          .then(continueRun)
-          .catch((err) => reject(err));
-
-      } else {
-
-        continueRun();
-
-      }
-
-    };
-
-    recur();
-
-  });
-
-};
 
 
 console.log(ClearScreen);
